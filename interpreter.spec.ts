15c7ea73667dea1521ee0a9353046a0bf19ea5e3
Gavin Zhang <gavin.g.zhang@gmail.com>
2020-03-03 21:12:11 +1100

    step 1: evaluate literal

--- interpreter.spec.ts
+++ interpreter.spec.ts
@@ -2,7 +2,11 @@ import { evaluate } from "./interpreter";
 import * as acorn from "acorn";
 
 test('integer literal', () => {
-  const source = '5';
+  const source = `
+7
+6
+5
+  `;
   const ast = acorn.parse(source);
   const actual = evaluate(ast);
   expect(actual).toBe(5);
--- interpreter.ts
+++ interpreter.ts
@@ -1,5 +1,32 @@
 import * as acorn from 'acorn';
 
-export function evaluate(ast: acorn.Node):any {
-    return 5;
+export function evaluate(node: any):any {
+    switch (node.type) {
+        case "Program":
+            return evalStatements(node.body);
+        case "ExpressionStatement":
+            return evalExpressionStatement(node);
+        case "Literal":
+            return evalLiteral(node);
+        default:
+            throw new Error("????");
+    }
+}
+
+function evalStatements(statements: any[]) {
+    let result
+    for (const statement of statements) {
+        result = evaluate(statement);
+        console.log(result);
+    }
+    // return the value of last statement
+    return result;
+}
+
+function evalExpressionStatement(node: any) {
+    return evaluate(node.expression);
+}
+
+function evalLiteral(node: any) {
+    return node.value;
 }
